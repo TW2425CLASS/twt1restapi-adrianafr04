eca44d1c55d32867bc8a60012b372fae12a9434d
adrianafr04 <adrianars@ipvc.pt>
2025-07-18 21:05:23 +0100

    tentando consertar vercel

--- backend/controllers/alunoController.js
+++ backend/controllers/alunoController.js
@@ -6,10 +6,17 @@ exports.getAllAlunos = async (req, res) => {
 };
 
 exports.createAluno = async (req, res) => {
-  const novoAluno = new Aluno(req.body);
-  await novoAluno.save();
-  res.status(201).json(novoAluno);
+  try {
+    const { _id, ...dadosAluno } = req.body;
+
+    const novoAluno = new Aluno(dadosAluno);
+    await novoAluno.save();
+    res.status(201).json(novoAluno);
+  } catch (error) {
+    res.status(400).json({ error: 'Erro ao criar aluno', details: error.message });
+  }
 };
+
 
 exports.updateAluno = async (req, res) => {
   const alunoAtualizado = await Aluno.findByIdAndUpdate(req.params.id, req.body, { new: true });
